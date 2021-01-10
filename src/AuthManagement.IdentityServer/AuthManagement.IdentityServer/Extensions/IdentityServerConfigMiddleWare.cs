fcd04f4742bac8b5376373a0d3d2f5be1fd3eb34
Yeasin Abedin <abedinyeasin@gmail.com>
2021-01-10 17:30:06 +0600

    Configure IdentityServer4 + Identity + EF to work together

---
+++ src/AuthManagement.IdentityServer/AuthManagement.IdentityServer/Extensions/IdentityServerConfigMiddleWare.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace AuthManagement.IdentityServer.Extensions
+{
+    public static class IdentityServerConfigMiddleWare
+    {
+        public static void ConfigureIdentityServer(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            services.AddIdentityServer()
+            // nuget for AddAspNetIdentity : IdentityServer4.AspNetIdentity
+            .AddAspNetIdentity<IdentityUser>()
+            // nuget for AddConfigurationStore : IdentityServer4.EntityFramework
+            .AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = builder => builder.UseSqlite(connectionString,
+                    opt => opt.MigrationsAssembly(migrationsAssembly));
+            })
+            // nuget for AddConfigurationStore : IdentityServer4.EntityFramework
+            .AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = builder => builder.UseSqlite(connectionString,
+                    opt => opt.MigrationsAssembly(migrationsAssembly));
+            })
+            .AddDeveloperSigningCredential();
+        }
+    }
+}
