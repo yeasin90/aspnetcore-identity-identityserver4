9d8ee148bfc455ca1feae8650f596c59304153c1
Yeasin Abedin <abedinyeasin@gmail.com>
2021-01-10 14:05:56 +0600

    Added SqLite configuration middleware

---
+++ src/AuthManagement.IdentityServer/AuthManagement.IdentityServer/Extensions/SqLiteMiddleWareExtension.cs
@@ -0,0 +1,18 @@
+﻿using AuthManagement.IdentityServer.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthManagement.IdentityServer.Extensions
+{
+    public static class SqLiteMiddleWareExtension
+    {
+        public static void ConfigureSqLite(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(connectionString));
+        }
+    }
+}
