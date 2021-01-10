d62c43ec72ab7c7c49b4207256e58f61150465d6
Yeasin Abedin <abedinyeasin@gmail.com>
2021-01-10 23:09:55 +0600

    Apply migrations on app start and before SeedDatabase

---
+++ src/AuthManagement.IdentityServer/Extensions/DbMigrationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using AuthManagement.IdentityServer.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AuthManagement.IdentityServer.Extensions
+{
+    public static class DbMigrationExtensions
+    {
+        public static void ApplyMigration<T>(this IHost host) where T : DbContext
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetService<T>();
+                context.Database.Migrate();
+            }
+        }
+    }
+}
