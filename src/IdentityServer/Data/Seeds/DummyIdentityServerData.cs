c812f72b8e26d9d9eda1a6b5f029a998e4304244
Yeasin Abedin <abedinyeasin@gmail.com>
2021-01-11 19:20:11 +0600

    Add clients for IdentityServer. Ref : https://vmsdurano.com/apiboilerplate-and-identityserver4-access-control-for-apis/

--- src/IdentityServer/Data/Seeds/DummyIdentityServerData.cs
+++ src/IdentityServer/Data/Seeds/DummyIdentityServerData.cs
@@ -35,40 +35,22 @@ namespace AuthManagement.IdentityServer.Data.Seeds
 
         public static List<Client> Clients => new List<Client>
             {
-                // m2m client credentials flow client
                 new Client
-                {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
-
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                    AllowedScopes = { "scope1" }
-                },
-
-                // interactive client using code flow + pkce
-                new Client
-                {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
-                    AllowedGrantTypes = GrantTypes.Code,
-
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
-                },
-                new Client
-                {
-                    ClientId = "testclient",
-                    ClientSecrets = new [] { new Secret("testSecret".Sha512()) },
-                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
-                    AllowedScopes = { "weather" }
-                },
+                    {
+                         ClientName = "Client Application1",
+                         ClientId = "t8agr5xKt4$3",
+                         AllowedGrantTypes = GrantTypes.ClientCredentials,
+                         ClientSecrets = { new Secret("eb300de4-add9-42f4-a3ac-abd3c60f1919".Sha256()) },
+                         AllowedScopes = new List<string> { "app.api.whatever.read", "app.api.whatever.write" }
+                    },
+                    new Client
+                    {
+                         ClientName = "Client Application2",
+                         ClientId = "3X=nNv?Sgu$S",
+                         AllowedGrantTypes = GrantTypes.ClientCredentials,
+                         ClientSecrets = { new Secret("1554db43-3015-47a8-a748-55bd76b6af48".Sha256()) },
+                         AllowedScopes = { "app.api.weather" }
+                    }
             };
     }
 }
