e56ade7fae0eaca6dd0aa8fff80697fb22fdbf05
Yeasin Abedin <abedinyeasin@gmail.com>
2021-01-11 20:01:30 +0600

    Add meaningfull IdentityServer config data

--- src/IdentityServer/Data/Seeds/DummyIdentityServerData.cs
+++ src/IdentityServer/Data/Seeds/DummyIdentityServerData.cs
@@ -8,20 +8,27 @@ namespace AuthManagement.IdentityServer.Data.Seeds
         public static IEnumerable<ApiResource> Apis =>
             new List<ApiResource>
             {
-                new ApiResource {
+                new ApiResource 
+                {
                     Name = "app.api.whatever",
                     DisplayName = "Whatever Apis",
                     ApiSecrets = { new Secret("a75a559d-1dab-4c65-9bc0-f8e590cb388d".Sha256()) },
                     Scopes = new List<string> { "app.api.whatever.read", "app.api.whatever.write", "app.api.whatever.full" }
                 },
-                new ApiResource("app.api.weather","Weather Apis")
+                new ApiResource()
+                {
+                    Name = "app.api.weather",
+                    DisplayName = "Weather Apis",
+                    ApiSecrets = { new Secret("E38171FD-C40A-40F1-9CBE-63C5F6D60AE8".Sha256()) },
+                    Scopes = new List<string> { "app.api.weather.read", "app.api.weather.write" }
+                }
             };
 
         public static List<Client> Clients => new List<Client>
             {
                 new Client
                     {
-                         ClientName = "Client Application1",
+                         ClientName = "Client for Whatever",
                          ClientId = "t8agr5xKt4$3",
                          AllowedGrantTypes = GrantTypes.ClientCredentials,
                          ClientSecrets = { new Secret("eb300de4-add9-42f4-a3ac-abd3c60f1919".Sha256()) },
@@ -29,11 +36,11 @@ namespace AuthManagement.IdentityServer.Data.Seeds
                     },
                     new Client
                     {
-                         ClientName = "Client Application2",
+                         ClientName = "Client for Weather",
                          ClientId = "3X=nNv?Sgu$S",
                          AllowedGrantTypes = GrantTypes.ClientCredentials,
                          ClientSecrets = { new Secret("1554db43-3015-47a8-a748-55bd76b6af48".Sha256()) },
-                         AllowedScopes = { "app.api.weather" }
+                         AllowedScopes = new List<string> { "app.api.weather.read" } // this client is only allowed to read Weather data
                     }
             };
     }
