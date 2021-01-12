2048264869967f72e57b56fbed72aa5ab198e2ef
Yeasin Abedin <abedinyeasin@gmail.com>
2021-01-12 23:47:45 +0600

    Added api versioning

---
+++ src/API.Hotels/Controllers/v2/HotelsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Hotels.Controllers.v2
+{
+    [ApiController]
+    [ApiVersion("1.0", Deprecated = true)]
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class HotelsController : ControllerBase
+    {
+
+    }
+}
