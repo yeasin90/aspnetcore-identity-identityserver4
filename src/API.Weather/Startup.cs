ce55417d2e05540dbe34af31614bf1264d149e96
Yeasin Abedin <abedinyeasin@gmail.com>
2021-01-12 22:05:37 +0600

    Remove boilerplates

--- src/API.Weather/Startup.cs
+++ src/API.Weather/Startup.cs
@@ -3,17 +3,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AuthManagement.Weather
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
