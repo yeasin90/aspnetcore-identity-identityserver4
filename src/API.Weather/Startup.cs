16aaf93768718b5ddbd6694b2c297f5e1803e657
Yeasin Abedin <abedinyeasin@gmail.com>
2021-01-12 21:56:32 +0600

    Refactoring and add two api Clients

--- src/API.Weather/Startup.cs
+++ src/API.Weather/Startup.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AuthManagement.Api
+namespace AuthManagement.Weather
 {
     public class Startup
     {
--- src/IdentityServer/Data/Seeds/DummyIdentityServerData.cs
+++ src/IdentityServer/Data/Seeds/DummyIdentityServerData.cs
@@ -8,9 +8,9 @@ namespace AuthManagement.IdentityServer.Data.Seeds
         public static IEnumerable<ApiScope> ApiScopes =>
             new List<ApiScope>
             {
-                new ApiScope("app.api.whatever.read"),
-                new ApiScope("app.api.whatever.write"),
-                new ApiScope("app.api.whatever.full"),
+                new ApiScope("app.api.hotels.read"),
+                new ApiScope("app.api.hotels.write"),
+                new ApiScope("app.api.hotels.full"),
                 new ApiScope("app.api.weather.read"),
                 new ApiScope("app.api.weather.write")
             };
@@ -20,15 +20,15 @@ namespace AuthManagement.IdentityServer.Data.Seeds
             {
                 new ApiResource
                 {
-                    Name = "app.api.whatever",
-                    DisplayName = "Whatever Apis",
+                    Name = "app.api.hotels",
+                    DisplayName = "Api Hotels",
                     ApiSecrets = { new Secret("a75a559d-1dab-4c65-9bc0-f8e590cb388d".Sha256()) },
-                    Scopes = new List<string> { "app.api.whatever.read", "app.api.whatever.write", "app.api.whatever.full" }
+                    Scopes = new List<string> { "app.api.hotels.read", "app.api.hotels.write", "app.api.hotels.full" }
                 },
                 new ApiResource()
                 {
                     Name = "app.api.weather",
-                    DisplayName = "Weather Apis",
+                    DisplayName = "Api Weather",
                     ApiSecrets = { new Secret("E38171FD-C40A-40F1-9CBE-63C5F6D60AE8".Sha256()) },
                     Scopes = new List<string> { "app.api.weather.read", "app.api.weather.write" }
                 }
@@ -38,15 +38,15 @@ namespace AuthManagement.IdentityServer.Data.Seeds
             {
                 new Client
                     {
-                         ClientName = "Client for Whatever",
+                         ClientName = "Client for Hotels Api",
                          ClientId = "t8agr5xKt4$3",
                          ClientSecrets = { new Secret("eb300de4-add9-42f4-a3ac-abd3c60f1919".Sha256()) },
                          AllowedGrantTypes = GrantTypes.ClientCredentials,
-                         AllowedScopes = new List<string> { "app.api.whatever.read", "app.api.whatever.write" },
+                         AllowedScopes = new List<string> { "app.api.hotels.read", "app.api.hotels.write" },
                     },
                     new Client
                     {
-                         ClientName = "Client for Weather",
+                         ClientName = "Client for Weather Api",
                          ClientId = "3X=nNv?Sgu$S",
                          ClientSecrets = { new Secret("1554db43-3015-47a8-a748-55bd76b6af48".Sha256()) },
                          AllowedGrantTypes = GrantTypes.ClientCredentials,
